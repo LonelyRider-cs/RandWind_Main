079726cee5263c0408c9a26f1cd58d162d9daea9
Luke Favret <luke.favret@colorado.edu>
2019-12-16 21:09:52 -0700

    changed res from send to jsonp

--- RandWind_local/server.js
+++ RandWind_local/server.js
@@ -389,7 +389,7 @@ app.post('/load_generations', function(req,res){
 	db.any('SELECT rand_string, string_id FROM random_strings WHERE user_email=$1', [req.session.userEmail])
 	.then(retrievedStrings => {
 		if (retrievedStrings) {
-			res.send(retrievedStrings);
+			res.jsonp(retrievedStrings);
 			return retrievedStrings;
 		  } else {
 			console.log("No strings present")
